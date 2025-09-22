98e923846e94d72ddc4c2c142a15172ca4b576bc
vladmirnyataige <vladmirnyataige1@gmail.com>
2025-09-22 03:11:14 +0300

    book preview made smaller

--- admin/src/components/Addbook.jsx
+++ admin/src/components/Addbook.jsx
@@ -1,231 +1,3 @@
-// import React, { useState } from "react";
-// import { styles } from "../assets/dummyStyles";
-// import axios from "axios";
-// import { BookPlus, Star } from "lucide-react";
-
-// const initialFormData = {
-//   title: "",
-//   author: "",
-//   price: "",
-//   image: null,
-//   rating: 4,
-//   category: "Fiction",
-//   description: "",
-//   preview: "",
-// };
-
-// const categories = [
-//   "Fiction",
-//   "Non-Fiction",
-//   "Mystery",
-//   "Sci-Fi",
-//   "Biography",
-//   "Self-Help",
-//   "Thriller",
-// ];
-// const API_BASE = "http://localhost:4000";
-// const Addbook = () => {
-//   const [formData, setformData] = useState(initialFormData);
-//   const [hoverRating, setHoverRating] = useState(0);
-//   const [loading, setLoading] = useState(false);
-//   const [message, setMessage] = useState({ type: null, text: null });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault(), setLoading(true);
-//     setMessage({ type: null, text: null });
-//     const payload = new FormData();
-//     Object.entries(formData).forEach(([key, value]) => {
-//       if (key !== "preview" && value !== null) payload.append(key, value);
-//     });
-//     try {
-//       await axios.post(`${API_BASE}/api/book`, payload);
-//       setMessage({ type: "success", text: "Book added successfully" });
-//       setformData(initialFormData);
-//     } catch (err) {
-//       console.error("Addbook error respond:", err.response?.data, err);
-//       setMessage({
-//         type: "error",
-//         text: err.response?.data?.message || "failed to add book",
-//       });
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setformData((prev) => ({ ...prev, [name]: value }));
-//   };
-//   //IMAGE HANDLING
-//   const handleImageChange = (e) => {
-//     const file = e.target.files[0];
-//     if (!file) return;
-
-//     setformData((prev) => ({
-//       ...prev,
-//       image: file,
-//       preview: URL.createObjectURL(file),
-//     }));
-//   };
-//   //   STARS RATING HANDLER
-//   const handleStarClick = (rating) => {
-//     setformData((prev) => ({ ...prev, rating }));
-//   };
-
-//   return (
-//     <div className={styles.addBooksPage}>
-//       <div className={styles.addBooksContainer}>
-//         <div className={styles.headerContainer}>
-//           <div>
-//             <h1 className={styles.headerTitle}>Add New Books</h1>
-//             <p className={styles.headerSubtitle}>
-//               Fill in the details to Add a new book to your store.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//       {/* FORM   */}
-//       <form onSubmit={handleSubmit} className={styles.formContainer}>
-//         <div className={styles.formGrid}>
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Book Title</label>
-//             <input
-//               name="title"
-//               value={formData.title}
-//               onChange={handleChange}
-//               className={styles.formInput}
-//               placeholder="Enter book Title"
-//               required
-//             />
-//           </div>
-
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Author</label>
-//             <input
-//               name="author"
-//               value={formData.author}
-//               onChange={handleChange}
-//               className={styles.formInput}
-//               placeholder="Enter Author Title"
-//               required
-//             />
-//           </div>
-
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Price(KES)</label>
-//             <input
-//               type="number"
-//               name="price"
-//               value={formData.price}
-//               onChange={handleChange}
-//               className={styles.formInput}
-//               placeholder="Enter price"
-//               required
-//             />
-//           </div>
-
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Rating</label>
-//             <div className={styles.ratingContainer}>
-//               <div className={styles.starContainer}>
-//                 {[1, 2, 3, 4, 5].map((starvalue) => (
-//                   <button
-//                     key={starvalue}
-//                     type="button"
-//                     onClick={() => handleStarClick(starvalue)}
-//                     onMouseEnter={() => setHoverRating(starvalue)}
-//                     onMouseLeave={() => setHoverRating(0)}
-//                     aria-label={`Rate ${starvalue} star${
-//                       starvalue !== 1 ? "s" : ""
-//                     }`}
-//                   >
-//                     <Star
-//                       className={`w-5 h-5 ${
-//                         (hoverRating || formData.rating) >= starvalue
-//                           ? styles.starFilled
-//                           : styles.starEmpty
-//                       }`}
-//                     />
-//                   </button>
-//                 ))}
-//               </div>
-//               <p className={styles.ratingText}>
-//                 {formData.rating} star{formData.rating != 1 ? "s" : ""}
-//               </p>
-//             </div>
-//           </div>
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Category</label>
-//             <select
-//               name="category"
-//               value={formData.category}
-//               onChange={handleChange}
-//               className={styles.formInput}
-//             >
-//               {categories.map((cat) => (
-//                 <option key={cat} value={cat}>
-//                   {cat}
-//                 </option>
-//               ))}
-//             </select>
-//           </div>
-//           <div className={styles.formItem}>
-//             <label className={styles.formLabel}>Cover Image</label>
-//             <input
-//               type="file"
-//               accept="image/*"
-//               onChange={handleImageChange}
-//               className={styles.formInput}
-//             />
-//           </div>
-//           <div className={`${styles.formItem} md:col-span-2`}>
-//             <label className={styles.formLabel}>Description</label>
-//             <textarea
-//               name="description"
-//               value={formData.description}
-//               onChange={handleChange}
-//               rows="4"
-//               className={styles.formTextarea}
-//               placeholder="Enter book Description"
-//             ></textarea>
-//           </div>
-//         </div>
-//         {formData.preview && (
-//           <div className={styles.previewContainer}>
-//             <h3 className={styles.previewTitle}>Cover Preview</h3>
-//             <img
-//               src={formData.preview}
-//               alt="image"
-//               className={styles.previewImg}
-//             />
-
-//             {message.text && (
-//               <p
-//                 className={`text-${
-//                   message.type === "success" ? "green" : "red"
-//                 }-500`}
-//               >
-//                 {message.text}
-//               </p>
-//             )}
-//           </div>
-//         )}
-//         <div className={styles.submitContainer}>
-//           <button
-//             disabled={loading}
-//             type="submit"
-//             className={styles.submitButton}
-//           >
-//             <BookPlus className="h-5 w-5" />
-//             <span>Add Book to Collection</span>
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Addbook;
 import React, { useState } from "react";
 import { styles } from "../assets/dummyStyles";
 import axios from "axios";
@@ -321,6 +93,7 @@ const Addbook = () => {
       {/* FORM   */}
       <form onSubmit={handleSubmit} className={styles.formContainer}>
         <div className={styles.formGrid}>
+          {/* Book Title */}
           <div className={styles.formItem}>
             <label className={styles.formLabel}>Book Title</label>
             <input
@@ -333,6 +106,7 @@ const Addbook = () => {
             />
           </div>
 
+          {/* Author */}
           <div className={styles.formItem}>
             <label className={styles.formLabel}>Author</label>
             <input
@@ -340,13 +114,14 @@ const Addbook = () => {
               value={formData.author}
               onChange={handleChange}
               className={styles.formInput}
-              placeholder="Enter Author Title"
+              placeholder="Enter Author Name"
               required
             />
           </div>
 
+          {/* Price */}
           <div className={styles.formItem}>
-            <label className={styles.formLabel}>Price(KES)</label>
+            <label className={styles.formLabel}>Price (KES)</label>
             <input
               type="number"
               name="price"
@@ -358,24 +133,25 @@ const Addbook = () => {
             />
           </div>
 
+          {/* Rating */}
           <div className={styles.formItem}>
             <label className={styles.formLabel}>Rating</label>
             <div className={styles.ratingContainer}>
-              <div className={styles.starContainer}>
-                {[1, 2, 3, 4, 5].map((starvalue) => (
+              <div className={styles.starsContainer}>
+                {[1, 2, 3, 4, 5].map((starValue) => (
                   <button
-                    key={starvalue}
+                    key={starValue}
                     type="button"
-                    onClick={() => handleStarClick(starvalue)}
-                    onMouseEnter={() => setHoverRating(starvalue)}
+                    onClick={() => handleStarClick(starValue)}
+                    onMouseEnter={() => setHoverRating(starValue)}
                     onMouseLeave={() => setHoverRating(0)}
-                    aria-label={`Rate ${starvalue} star${
-                      starvalue !== 1 ? "s" : ""
+                    aria-label={`Rate ${starValue} star${
+                      starValue !== 1 ? "s" : ""
                     }`}
                   >
                     <Star
                       className={`w-5 h-5 ${
-                        (hoverRating || formData.rating) >= starvalue
+                        (hoverRating || formData.rating) >= starValue
                           ? styles.starFilled
                           : styles.starEmpty
                       }`}
@@ -384,11 +160,12 @@ const Addbook = () => {
                 ))}
               </div>
               <p className={styles.ratingText}>
-                {formData.rating} star{formData.rating != 1 ? "s" : ""}
+                {formData.rating} star{formData.rating !== 1 ? "s" : ""}
               </p>
             </div>
           </div>
 
+          {/* Category */}
           <div className={styles.formItem}>
             <label className={styles.formLabel}>Category</label>
             <select
@@ -405,6 +182,7 @@ const Addbook = () => {
             </select>
           </div>
 
+          {/* Cover Image */}
           <div className={styles.formItem}>
             <label className={styles.formLabel}>Cover Image</label>
             <input
@@ -415,6 +193,7 @@ const Addbook = () => {
             />
           </div>
 
+          {/* Description */}
           <div className={`${styles.formItem} md:col-span-2`}>
             <label className={styles.formLabel}>Description</label>
             <textarea
@@ -428,28 +207,32 @@ const Addbook = () => {
           </div>
         </div>
 
+        {/* Image Preview */}
         {formData.preview && (
           <div className={styles.previewContainer}>
             <h3 className={styles.previewTitle}>Cover Preview</h3>
-            <img
-              src={formData.preview}
-              alt="image"
-              className={styles.previewImg}
-            />
+            <div className={styles.previewImage}>
+              <img
+                src={formData.preview}
+                alt="image"
+                className={styles.previewImg}
+              />
+            </div>
           </div>
         )}
 
-        {/* ✅ Message always visible */}
+        {/* ✅ Fixed Tailwind dynamic class */}
         {message.text && (
           <p
-            className={`mt-2 text-center text-${
-              message.type === "success" ? "green" : "red"
-            }-500`}
+            className={`mt-2 text-center ${
+              message.type === "success" ? "text-green-500" : "text-red-500"
+            }`}
           >
             {message.text}
           </p>
         )}
 
+        {/* Submit */}
         <div className={styles.submitContainer}>
           <button
             disabled={loading}
