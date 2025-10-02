af3a9f4d83da4ecc82028561fc47e7a2020a7c95
vladmirnyataige <vladmirnyataige1@gmail.com>
2025-10-02 18:52:49 +0300

    Update OurBestSellers.jsx

--- frontend/src/components/OurBestSellers.jsx
+++ frontend/src/components/OurBestSellers.jsx
@@ -12,7 +12,7 @@ import { bgColors, obsbooks } from "../assets/dummydata";
 import { useCart } from "../CartContext/CartContext";
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://bookshell-6mg7.onrender.com";
 
 const OurBestSellers = () => {
   const scrollRef = useRef(null);
